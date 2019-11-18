2aa2fe0cd72d89ad9311c64d2b18ba967f9cbf4f
Pak Kian, Lew <pakkian.lew@pakkianlew.com>
2019-11-18 20:36:35 +0800

    Updates

---
+++ precache-manifest.1d975858edf97166e673b5a1c91a63ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222bdb3f7a83fb0f2bfdd3351c8a2f5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6ac8da67281e51ced42",
+    "url": "/static/css/main.c73d7172.chunk.css"
+  },
+  {
+    "revision": "3f6c8580d6bc7a7683cd",
+    "url": "/static/js/2.3349f53f.chunk.js"
+  },
+  {
+    "revision": "f6ac8da67281e51ced42",
+    "url": "/static/js/main.0c25a9e7.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
