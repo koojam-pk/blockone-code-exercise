8f84a5f48a7e369f6e872f71ee675c63abf26246
Pak Kian, Lew <pakkian.lew@pakkianlew.com>
2019-11-18 20:43:14 +0800

    Updates

---
+++ precache-manifest.941ad5296120a2988203a9b8cf659161.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0545b29a350d5596f9b0ff4fe56863f6",
+    "url": "/blockone-code-exercise/index.html"
+  },
+  {
+    "revision": "e769ee91750810752fd3",
+    "url": "/blockone-code-exercise/static/css/main.c73d7172.chunk.css"
+  },
+  {
+    "revision": "cdce31f8fd4e59679d34",
+    "url": "/blockone-code-exercise/static/js/2.9b770234.chunk.js"
+  },
+  {
+    "revision": "e769ee91750810752fd3",
+    "url": "/blockone-code-exercise/static/js/main.acafee02.chunk.js"
+  },
+  {
+    "revision": "aaa5dd1dde46bf956d3f",
+    "url": "/blockone-code-exercise/static/js/runtime-main.d84d165d.js"
+  }
+]);
